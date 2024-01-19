a684636424318091333dd03ecf0388e0ef0a5e2e
Ömer Birler <omer.birler@outlook.com>
2024-01-20 00:27:42 +0300

    Add project files.

---
+++ StlToInpConverter/Part.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CustomCollections;
+
+using devDept.Geometry;
+
+namespace StlToInpConverter
+{
+    internal class Part
+    {
+        public Part()
+        {
+            Nodes = new BiDictionary<int, Point3D>();
+            Triangles = new List<IndexTriangle>();
+        }
+
+        public Part(string name)
+        {
+            Name = name;
+            Nodes = new BiDictionary<int, Point3D>();
+            Triangles = new List<IndexTriangle>();
+        }
+
+        public string Name;
+
+        public BiDictionary<int, Point3D> Nodes;
+
+        public List<IndexTriangle> Triangles;
+    }
+}
